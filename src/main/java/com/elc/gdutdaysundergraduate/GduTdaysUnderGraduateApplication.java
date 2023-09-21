ba7dbe4b3ba51f9e87e5decd33528e74061961e6
单良光 <3122002311@mail2.gdut.edu.cn>
2023-09-21 21:41:42 +0800

    Add files via upload

---
+++ src/main/java/com/elc/gdutdaysundergraduate/GduTdaysUnderGraduateApplication.java
@@ -0,0 +1,13 @@
+package com.elc.gdutdaysundergraduate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GduTdaysUnderGraduateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GduTdaysUnderGraduateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elc/gdutdaysundergraduate/GduTdaysUnderGraduateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elc.gdutdaysundergraduate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GduTdaysUnderGraduateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
