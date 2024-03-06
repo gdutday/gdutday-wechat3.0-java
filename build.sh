5cebaefae9b7fc3e7da9f3f8b75c9d5f548c5f5e
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2024-03-07 01:17:43 +0800

    fix( build.sh ): 修改部署脚本
    修改部署的镜像名
    
    Signed-off-by: 李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ VERSION=$1
 cd ./ || exit
 
 # 构建 Docker 镜像，并将版本号作为标签
-docker build -t reg.gdutelc.com/gdutday/gdutday:"$VERSION" .
+docker build -t registry.gdutelc.com/gdutday/gdutday:"$VERSION" .
 
 # 检查构建是否成功
 if [ $? -ne 0 ]; then
@@ -22,14 +22,14 @@ if [ $? -ne 0 ]; then
 fi
 
 # 给镜像加上 latest 标签
-docker tag reg.gdutelc.com/gdutday/gdutday:"$VERSION" registry.gdutelc.com/gdutday/gdutday:latest
+docker tag registry.gdutelc.com/gdutday/gdutday:"$VERSION" registry.gdutelc.com/gdutday/gdutday:latest
 
 # 登录 Docker 注册表（请替换为实际的用户名、密码和注册表地址）
 # echo "your-username" | docker login -u "your-username" --password-stdin registry.example.com
 
 # 将镜像推送到 Docker 注册表
-docker push reg.gdutelc.com/gdutday/gdutday:"$VERSION"
-docker push reg.gdutelc.com/gdutday/gdutday:latest
+docker push registry.gdutelc.com/gdutday/gdutday:"$VERSION"
+docker push registry.gdutelc.com/gdutday/gdutday:latest
 
 # 如果推送成功，则输出提示信息
 if [ $? -eq 0 ]; then
